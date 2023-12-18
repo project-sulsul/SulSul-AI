068d3a017f19759facb6b3c2e722fc6fc8d2a6a0
Sangh0 <hop7311@naver.com>
2023-12-19 03:20:58 +0900

    feat: inference on a image from url

--- inference.py
+++ inference.py
@@ -1,5 +1,8 @@
 import argparse
+import requests
+import numpy as np
 from PIL import Image
+from io import BytesIO
 from typing import *
 
 import torch
@@ -11,72 +14,88 @@ from utils.dataset import Padding
 from quantization.quantize import ptq_serving, qat_serving
 
 
-classes = {
-    0: '소고기', 1: '맥주', 2: '치킨', 3: '닭발', 4: '닭갈비',
-    5: '마른안주', 6: '두부김치', 7: '황도', 8: '계란말이', 9: '어묵탕',
-    10: '감자튀김', 11: '곱창', 12: '하이볼', 13: '화채', 14: '짬뽕',
-    15: '짜파게티', 16: '라면', 17: '양꼬치', 18: '나초', 19: '나가사키 짬뽕',
-    20: '피자', 21: '삼겹살', 22: '족발', 23: '육회', 24: '연어',
-    25: '회', 26: '새우튀김', 27: '소주',
+class_info = {
+    # foods
+    'beef': 0, 'chicken': 1, 'chicken_feet': 2, 'chicken_ribs': 3, 'dry_snacks': 4,
+    'dubu_kimchi': 5, 'ecliptic': 6, 'egg_roll': 7, 'fish_cake_soup': 8,
+    'french_fries': 9, 'gopchang': 10, 'hwachae': 11, 'jjambbong': 12,
+    'jjapageti': 13, 'korean_ramen': 14, 'lamb_skewers': 15, 'nacho': 16,
+    'nagasaki': 17, 'pizza': 18, 'pork_belly': 19, 'pork_feet': 20,
+    'raw_meat': 21, 'salmon': 22, 'sashimi': 23, 'shrimp_tempura': 24,
+    
+    # drinks
+    'beer': 25, 'cass': 26, 'chamisul_fresh': 27, 'chamisul_origin': 28,
+    'chum_churum': 29, 'highball': 30, 'hite': 31, 'jinro': 32, 'kelly': 33, 
+    'kloud': 34, 'ob': 35, 'saero': 36, 'soju': 37, 'tera': 38,
 }
+
+class_info_rev = {v: k for k, v in class_info.items()}
 
 
 transformation = transforms.Compose([
     Padding(fill=(0, 0, 0)),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
 
-def load_image(src: str):    
-    img = Image.open(src).convert('RGB')
+def load_image(img_url: str):
+    response = requests.get(img_url)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
     img = transformation(img)
     img = img.unsqueeze(dim=0)
-    return img, src
+    return img, img_url
 
 
-def inference(src: torch.Tensor, model: nn.Module):
+def inference(src: torch.Tensor, model: nn.Module, threshold: int=0.5):
     model.eval()
+    result_list = []
     with torch.no_grad():
         outputs = model(src)
-        # outputs = F.softmax(outputs)
-        result = classes[torch.argmax(outputs, dim=1).item()]
-    return result
+        result = outputs[0].detach().numpy()
+        indices = np.where(result > threshold)[0]
+        for idx in indices:
+            result_list.append(class_info_rev[int(idx)])
+    return result_list
 
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Inference', add_help=False)
     parser.add_argument('--model_name', type=str, required=True,
-                        choices=['shufflenet', 'resnet18', 'resnet50'],
                         help='model name')
-    parser.add_argument('--src', type=str, required=True,
+    parser.add_argument('--img_url', type=str, required=True,
                         help='input image')
     parser.add_argument('--weight', type=str, required=True,
                         help='a path of trained weight file')
-    parser.add_argument('--quantization', type=str, default='none', 
-                        choices=['none', 'qat', 'ptq'],
-                        help='load quantized model or float32 model')
-    parser.add_argument('--num_classes', type=int, default=28,
+    parser.add_argument('--num_classes', type=int, default=25,
                         help='the number of classes')
+    parser.add_argument('--quantization', type=str, default='none', choices=['none', 'qat', 'ptq'],
+                        help='load quantized model or float32 model')
+    parser.add_argument('--measure_latency', action='store_true',
+                        help='print latency time')
+    parser.add_argument('--threshold', type=float, default=0.5,
+                        help='a threshold for filtering outputs')
+
     return parser
 
 
 def main(args):
-    q = True if args.quantization is not 'none' else False
+    q = True if args.quantization != 'none' else False
 
     # load model
     if args.model_name == 'shufflenet':
         from models.shufflenet import ShuffleNetV2
         model = ShuffleNetV2(num_classes=args.num_classes, pre_trained=False, quantize=q)
-
+        
     elif args.model_name == 'resnet18':
         from models.resnet import resnet18
         model = resnet18(num_classes=args.num_classes, pre_trained=False, quantize=q)
-
+        
     elif args.model_name == 'resnet50':
         from models.resnet import resnet50
         model = resnet50(num_classes=args.num_classes, pre_trained=False, quantize=q)
-
+        
     else:
         raise ValueError(f'model name {args.model_name} does not exists.')
     
@@ -91,8 +110,8 @@ def main(args):
     else: # 'none'
         pass
 
-    img, _ = load_image(args.src)
-    result = inference(img, model)
+    img, _ = load_image(args.img_url)
+    result = inference(img, model, args.threshold)
     print(result)
 
 
